41f02817dcfd3a48c6e33236fbfdc2cbd428dd03
haerang <gofkdvjvl@naver.com>
2020-06-07 06:19:41 +0900

    UPdate

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var crypto = require('crypto');
+var crypto = require("crypto");
 
 const UserModel = require("../models/user");
 const encrypt = require("../modules/crypto");
@@ -25,17 +25,9 @@ router.post("/signup", async (req, res) => {
       .send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
     return;
   }
-  // const {
-  //   salt,
-  //   hashed
-  // } = await encrypt.encrypt(password);
-  // const idx = await UserModel.signup(id, hashed, salt);
-  
-  const salt = crypto.pbkdf2Sync(32).toString('hex');
-  const hashed = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha512').toString();
-  
+  const { salt, hashed } = await encrypt.encrypt(password);
   const idx = await UserModel.signup(id, hashed, salt);
-  
+
   if (idx === -1) {
     return res
       .status(statusCode.DB_ERROR)
@@ -67,11 +59,7 @@ router.post("/signin", async (req, res) => {
   }
   // req의 Password 확인 - 틀렸다면 MISS_MATCH_PW 반납
   // encrypt 모듈 만들어놓은 거 잘 활용하기!
-  // const hashed = await encrypt.encryptWithSalt(password, user[0].salt);
-  // console.log(hashed);
-  // console.log(user[0]);
-
-  const hashed = crypto.pbkdf2Sync(password, user[0].salt, 100000, 32, 'sha512').toString();
+  const hashed = await encrypt.encryptWithSalt(password, user[0].salt);
 
   if (hashed !== user[0].password) {
     return res
@@ -90,19 +78,19 @@ router.post("/signin", async (req, res) => {
   );
 });
 
-// 마이페이지 조회 
+// 마이페이지 조회
 // 댓글, 좋아요 누른 뮤직 리스트 반환
-router.get("/mypage", async (req, res) => {
-  const mpHeart = await UserModel.heartSearch();
-  const mpComments = await UserModel.commentSearch();
+// router.get("/mypage", async (req, res) => {
+//   const mpHeart = await UserModel.heartSearch();
+//   const mpComments = await UserModel.commentSearch();
 
-  if (mpHeart.length < 1 && mpComments.length < 1) {
-    return res.status(statusCode.BAD_REQUEST)
-    .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_MYPAGE_DATA));
-  }
+//   if (mpHeart.length < 1 && mpComments.length < 1) {
+//     return res.status(statusCode.BAD_REQUEST)
+//     .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_COMMENTS_DATA));
+//   }
 
-  res.status(statusCode.OK)
-  .send(util.success(statusCode.OK, resMessage.MYPAGE_SUCCESS, {mpHeart: mpHeart, mpComments: mpComments}));
-})
+//   res.status(statusCode.OK)
+//   .send(util.success(statusCode.OK, resMessage.COMMENTS_SUCCESS, {mpHeart: mpHeart, mpComments: mpComments}));
+// });
 
 module.exports = router;

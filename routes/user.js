8d36b53a594cb9a72130ba66ef48a8e55fc7ee1a
rkdls8268 <rkdls8268@gmail.com>
2020-06-07 06:16:54 +0900

    update signin

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const encrypt = require("../modules/crypto");
 const statusCode = require("../modules/statusCode");
 const resMessage = require("../modules/responseMessage");
 const util = require("../modules/util");
-// const jwt = require('../modules/jwt');
+const jwt = require('../modules/jwt');
 
 router.post("/signup", async (req, res) => {
   const { id, password } = req.body;
@@ -25,15 +25,10 @@ router.post("/signup", async (req, res) => {
       .send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
     return;
   }
-  // const {
-  //   salt,
-  //   hashed
-  // } = await encrypt.encrypt(password);
-  // const idx = await UserModel.signup(id, hashed, salt);
-  
-  const salt = crypto.pbkdf2Sync(32).toString('hex');
-  const hashed = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha512').toString();
-  
+  const {
+    salt,
+    hashed
+  } = await encrypt.encrypt(password);
   const idx = await UserModel.signup(id, hashed, salt);
   
   if (idx === -1) {
@@ -67,11 +62,7 @@ router.post("/signin", async (req, res) => {
   }
   // req의 Password 확인 - 틀렸다면 MISS_MATCH_PW 반납
   // encrypt 모듈 만들어놓은 거 잘 활용하기!
-  // const hashed = await encrypt.encryptWithSalt(password, user[0].salt);
-  // console.log(hashed);
-  // console.log(user[0]);
-
-  const hashed = crypto.pbkdf2Sync(password, user[0].salt, 100000, 32, 'sha512').toString();
+  const hashed = await encrypt.encryptWithSalt(password, user[0].salt);
 
   if (hashed !== user[0].password) {
     return res
@@ -79,31 +70,14 @@ router.post("/signin", async (req, res) => {
       .send(util.fail(statusCode.BAD_REQUEST, resMessage.MISS_MATCH_PW));
   }
 
-  // console.log(user[0]);
   // 로그인 성공적으로 마쳤다면 - LOGIN_SUCCESS 전달
-  // const {token, _} = await jwt.sign(user[0]);
+  const {token, _} = await jwt.sign(user[0]);
 
   res.status(statusCode.OK).send(
     util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, {
-      useridx: user[0],
+      token: token
     })
   );
 });
-
-// 마이페이지 조회 
-// 댓글, 좋아요 누른 뮤직 리스트 반환
-// router.get("/mypage", async (req, res) => {
-//   const mpHeart = await UserModel.heartSearch();
-//   const mpComments = await UserModel.commentSearch();
-
-//   if (mpHeart.length < 1 && mpComments.length < 1) {
-//     return res.status(statusCode.BAD_REQUEST)
-//     .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_COMMENTS_DATA));
-//   }
-
-//   res.status(statusCode.OK)
-//   .send(util.success(statusCode.OK, resMessage.COMMENTS_SUCCESS, {mpHeart: mpHeart, mpComments: mpComments}));
-// });
-
 
 module.exports = router;

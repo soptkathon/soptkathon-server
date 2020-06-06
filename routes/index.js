a8961619eeccc900ea3c0e1d9b34af7ffc093d62
haerang <gofkdvjvl@naver.com>
2020-06-06 19:54:11 +0900

    Made first repo

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const result = {
+    status: 200,
+    message: "api test",
+  };
+  res.status(200).send(result);
+});
+
+router.use("/user", require("./user"));
+
+module.exports = router;
